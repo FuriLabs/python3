548384f46185c0cfbce83831185b34d368515c4e
Bardia Moshiri <bardia@furilabs.com>
2024-09-30 16:23:52 -0400

    TEMP: merge debian patches
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- Lib/argparse.py
+++ Lib/argparse.py
@@ -91,7 +91,16 @@ import sys as _sys
 
 import warnings
 
-from gettext import gettext as _, ngettext
+try:
+    from gettext import gettext as _, ngettext
+except ImportError:
+    def _(message):
+        return message
+    def ngettext(singular,plural,n):
+        if n == 1:
+            return singular
+        else:
+            return plural
 
 SUPPRESS = '==SUPPRESS=='
 
@@ -169,9 +178,12 @@ class HelpFormatter(object):
 
         # default setting for width
         if width is None:
-            import shutil
-            width = shutil.get_terminal_size().columns
-            width -= 2
+            try:
+                import shutil as _shutil
+                width = _shutil.get_terminal_size().columns
+                width -= 2
+            except ImportError:
+                width = 70
 
         self._prog = prog
         self._indent_increment = indent_increment
